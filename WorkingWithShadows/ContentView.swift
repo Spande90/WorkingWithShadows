c944422145422026e26975ff79926d13c721b7e1
Siddharth Pande <siddharth.v.pande@gmail.com>
2021-04-20 22:00:39 +0530

    Initial Commit

---
+++ WorkingWithShadows/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkingWithShadows
+//
+//  Created by Siddharth on 20/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
